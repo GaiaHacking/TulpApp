4db9342da5a8627e332c4b7276ebe76bdaf035a9
TheGaiaSystem <thegaiasystem@gmail.com>
2021-04-07 16:39:34 +0200

    Started to work on the single chat page

--- src/app/tulpa-chat-page/tulpa-chat-page.page.ts
+++ src/app/tulpa-chat-page/tulpa-chat-page.page.ts
@@ -6,9 +6,17 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './tulpa-chat-page.page.html',
   styleUrls: ['./tulpa-chat-page.page.scss'],
 })
+
 export class TulpaChatPagePage implements OnInit {
 
-  tulpaID = null;
+  tulpaID = '';
+
+  //this array will contain the messages that will be read out from the database later
+  chatMessages = [
+    {sender: this.tulpaID, messageContent: "I think that you should help them", date: new Date("December 9, 2019 09:30:30")},
+    {sender: "Host", messageContent: "I do not think that this is so easy", date: new Date("December 9, 2019 09:31:00")}
+  ];
+  currentWriter = this.tulpaID; //this will be used to determine which one is writing at the moment
 
   constructor(private route: ActivatedRoute) { }
 
